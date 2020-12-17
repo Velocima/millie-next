fb3c60c781d9eba156d7350203d16637407681d8
Velo <hartley.max@outlook.com>
2020-12-17 01:08:16 +0000

    collapsible nav functionality restored from original site. css cleaned. useWindowSize hook added for edgecase resizing bugs

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,9 +1,35 @@
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import useWindowSize from '../lib/useWindowSize';
 import style from '../styles/nav.module.css';
 import { FaFacebookF, FaInstagram } from 'react-icons/fa';
 import Icon from './icon';
 
 export default function Nav() {
+	const [width, height] = useWindowSize();
+	const [isNavOpen, setIsNavOpen] = useState(false);
+
+	const handleBurgerClick = () => {
+		setIsNavOpen((prevState) => !prevState);
+	};
+
+	useEffect(() => {
+		if (width > 1050) {
+			setIsNavOpen(true);
+		} else {
+			setIsNavOpen(false);
+		}
+	}, [width, height]);
+
+	const burgerStyle = {
+		transform: isNavOpen ? 'rotate(180deg)' : 'none',
+	};
+
+	const menuStyle = {
+		transform: isNavOpen ? 'translateY(0)' : 'translateY(-100%)',
+		opacity: isNavOpen ? 1 : 0,
+	};
+
 	return (
 		<nav className={style.nav}>
 			<div className={style.logo}>
@@ -21,12 +47,12 @@ export default function Nav() {
 					</Icon>
 				</div>
 			</div>
-			<div className={style.burger}>
+			<div className={style.burger} onClick={handleBurgerClick} style={burgerStyle}>
 				<div className={style.line}></div>
 				<div className={style.line}></div>
 				<div className={style.line}></div>
 			</div>
-			<ul className={`menu ${style.list}`}>
+			<ul className={style.menu} style={menuStyle}>
 				<li className={style.listItem}>
 					<Link href='/about'>
 						<a>ABOUT</a>
