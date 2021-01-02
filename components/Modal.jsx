56c057d78bd8ee504bd1f8b2d3814b1be4c79ae6
Velo <hartley.max@outlook.com>
2021-01-02 02:15:03 +0000

    modal functionality restored to original design

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,14 +1,37 @@
 import style from '../styles/modal.module.css';
 
-export default function Modal({ image, isVisible }) {
+export default function Modal({ images, isVisible, onClose, nextImage, prevImage, isFirstImage }) {
+	const modalStyle = {
+		opacity: isVisible ? 1 : 0,
+		pointerEvents: isVisible ? 'all' : 'none',
+	};
+	const imageOneStyle = {
+		opacity: isFirstImage ? 1 : 0,
+	};
+	const imageTwoStyle = {
+		opacity: isFirstImage ? 0 : 1,
+	};
 	return (
-		<div className='modal'>
-			<div className='close'>X</div>
-			<div className='left'>
+		<div className={style.modal} style={modalStyle}>
+			<div className={style.close} onClick={onClose}>
+				X
+			</div>
+			<div className={style.left} onClick={prevImage}>
 				<span>&lt;</span>
 			</div>
-			<img className='image-1' src={image} alt='' />
-			<div className='right'>
+			<img
+				className={style.image}
+				src={images.imageOne.image}
+				alt={images.imageOne.alt}
+				style={imageOneStyle}
+			/>
+			<img
+				className={style.image}
+				src={images.imageTwo.image}
+				alt={images.imageTwo.alt}
+				style={imageTwoStyle}
+			/>
+			<div className={style.right} onClick={nextImage}>
 				<span>&gt;</span>
 			</div>
 		</div>
--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Nav from '../components/nav';
 import GallerySection from '../components/gallerySection';
 import Modal from '../components/Modal';
+import { useState, useEffect } from 'react';
+import next from 'next';
 
 export default function Gallery() {
 	const gallerySection1 = [
@@ -59,6 +61,109 @@ export default function Gallery() {
 		{ image: '/images/HENNA/124.jpg', alt: '' },
 		{ image: '/images/HENNA/grey-6.jpg', alt: '' },
 	];
+
+	const allGalleryImages = [
+		...gallerySection1,
+		...gallerySection2,
+		...gallerySection3,
+		...gallerySection4,
+		...gallerySection5,
+	];
+
+	// Modal related state and functions
+	const [modalImage, setModalImage] = useState({
+		imageOne: { image: '', alt: '' },
+		imageTwo: { image: '', alt: '' },
+	});
+	const [isImageChanging, setIsImageChanging] = useState(false);
+	const [isFirstImage, setIsFirstImage] = useState(true);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleCloseModal = () => {
+		setIsFirstImage(true);
+		setIsModalOpen(false);
+	};
+
+	const handleImageClick = ({ target }) => {
+		setIsFirstImage(true);
+		setModalImage((prevState) => {
+			return { ...prevState, imageOne: { image: target.src, alt: target.alt } };
+		});
+		setIsModalOpen(true);
+	};
+
+	const getNextImage = () => {
+		const currentImageSrc = isFirstImage
+			? modalImage.imageOne.image
+			: modalImage.imageTwo.image;
+		let index = allGalleryImages.findIndex((image) => image.image === currentImageSrc);
+		if (index === allGalleryImages.length - 1) {
+			return allGalleryImages[0];
+		} else {
+			return allGalleryImages[index + 1];
+		}
+	};
+	const getPrevImage = () => {
+		const currentImageSrc = isFirstImage
+			? modalImage.imageOne.image
+			: modalImage.imageTwo.image;
+		let index = allGalleryImages.findIndex((image) => image.image === currentImageSrc);
+
+		if (index === 0) {
+			return allGalleryImages[allGalleryImages.length - 1];
+		} else {
+			return allGalleryImages[index - 1];
+		}
+	};
+
+	const handleModalRightClick = () => {
+		if (isImageChanging) return;
+		const nextImage = getNextImage();
+		setIsImageChanging(true);
+		if (isFirstImage) {
+			setModalImage((prevState) => {
+				return { ...prevState, imageTwo: { image: nextImage.image, alt: nextImage.alt } };
+			});
+		} else {
+			setModalImage((prevState) => {
+				return { ...prevState, imageOne: { image: nextImage.image, alt: nextImage.alt } };
+			});
+		}
+		setIsFirstImage((prevState) => !prevState);
+		setTimeout(() => {
+			setIsImageChanging(false);
+		}, 500);
+	};
+	const handleModalLeftClick = () => {
+		if (isImageChanging) return;
+		const prevImage = getPrevImage();
+		setIsImageChanging(true);
+		if (isFirstImage) {
+			setModalImage((prevState) => {
+				return { ...prevState, imageTwo: { image: prevImage.image, alt: prevImage.alt } };
+			});
+		} else {
+			setModalImage((prevState) => {
+				return { ...prevState, imageOne: { image: prevImage.image, alt: prevImage.alt } };
+			});
+		}
+		setIsFirstImage((prevState) => !prevState);
+		setTimeout(() => {
+			setIsImageChanging(false);
+		}, 500);
+	};
+
+	useEffect(() => {
+		const onKeyPress = ({ key }) => {
+			if (key === 'ArrowLeft') handleModalLeftClick();
+			if (key === 'ArrowRight') handleModalRightClick();
+			if (key === 'Escape') handleCloseModal();
+		};
+		document.addEventListener('keydown', onKeyPress);
+		return () => {
+			document.removeEventListener('keydown', onKeyPress);
+		};
+	}, [handleCloseModal, handleModalLeftClick, handleModalRightClick]);
 	return (
 		<>
 			<Head>
@@ -69,30 +174,37 @@ export default function Gallery() {
 			<div className='banner'>
 				<h1>GALLERY</h1>
 			</div>
-			<GallerySection images={gallerySection1} onClick={null} />
+			<GallerySection images={gallerySection1} onClick={handleImageClick} />
 			<div className='headline headline-1'>
 				<h2>UNIQUE SETUP</h2>
 				<h4>VINTAGE VW CAMPERVAN</h4>
 			</div>
-			<GallerySection images={gallerySection2} onClick={null} />
+			<GallerySection images={gallerySection2} onClick={handleImageClick} />
 			<div className='headline headline-2'>
 				<h2>BESPOKE DESIGNS</h2>
 				<h4>FOR EVERY OCCASION</h4>
 			</div>
-			<GallerySection images={gallerySection3} onClick={null} />
+			<GallerySection images={gallerySection3} onClick={handleImageClick} />
 			<div className='quote headline-3 headline'>
 				<h4>“Beautifully intricate henna tattoo, leaving a lovely defined stain.”</h4>
 				<p>— Caroline Scamell</p>
 			</div>
-			<GallerySection images={gallerySection4} onClick={null} />
+			<GallerySection images={gallerySection4} onClick={handleImageClick} />
 			<div className='headline-4 quote headline'>
 				<h4>
 					“I can’t quite describe Millie’s amazing talent with henna, she is phenomenal. ”
 				</h4>
 				<p>— Hannah Jones</p>
 			</div>
-			<GallerySection images={gallerySection5} onClick={null} />
-			<Modal image={null} isVisible={null} />
+			<GallerySection images={gallerySection5} onClick={handleImageClick} />
+			<Modal
+				images={modalImage}
+				isFirstImage={isFirstImage}
+				isVisible={isModalOpen}
+				onClose={handleCloseModal}
+				prevImage={handleModalLeftClick}
+				nextImage={handleModalRightClick}
+			/>
 		</>
 	);
 }
